55f30e6fd57753658007a4809936ccc6ae23e83e
JUNU <bokjunwoo@gmail.com>
2023-03-01 13:11:45 +0900

    data : 임이의 pickData 추가(SubMain에서 사용)

--- data/region.ts
+++ data/region.ts
@@ -41,24 +41,58 @@ export const mainCardData: MainCardData[] = [
     id: 0,
     image: '/images/main/card0.jpg',
     text: '오랜만의 서울여행! 여행 포인트',
-    region: 'seoul'
+    region: 'seoul',
   },
   {
     id: 1,
     image: '/images/main/card1.jpg',
     text: '고즈넉한 가을 감성 강릉의 대표 명소',
-    region: 'gangneung'
+    region: 'gangneung',
   },
   {
     id: 2,
     image: '/images/main/card2.jpg',
     text: "요즘 부산 가면 '이곳'에 꼭 들린대요",
-    region: 'busan'
+    region: 'busan',
   },
   {
     id: 3,
     image: '/images/main/card3.jpg',
     text: '잘 몰랐던 경주 지금이 떠날 기회!',
-    region: 'gyeongju'
+    region: 'gyeongju',
+  },
+];
+
+export interface PickData {
+  id: number;
+  title: string;
+  image: string;
+}
+
+export const pickData: PickData[] = [
+  {
+    id: 1,
+    title: '산과 바다',
+    image: 'https://picsum.photos/id/237/200/300',
+  },
+  {
+    id: 2,
+    title: '도심 속 휴식',
+    image: 'https://picsum.photos/id/238/200/300',
+  },
+  {
+    id: 3,
+    title: '자연과 함께하는 여행',
+    image: 'https://picsum.photos/id/239/200/300',
+  },
+  {
+    id: 4,
+    title: '역사와 문화',
+    image: 'https://picsum.photos/id/240/200/300',
+  },
+  {
+    id: 5,
+    title: '여유로운 시간',
+    image: 'https://picsum.photos/id/241/200/300',
   },
 ];
