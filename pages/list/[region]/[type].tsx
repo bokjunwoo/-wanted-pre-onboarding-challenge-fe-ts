fcb3b7578b428f0b70a87f0e348eb9f575de7611
JUNU <bokjunwoo@gmail.com>
2023-03-03 01:28:54 +0900

    fix : 폴더구조 변경

---
+++ pages/list/[region]/[type].tsx
@@ -0,0 +1,54 @@
+import ListCard from '@/components/list/ListCard';
+import CommonNav from '@/components/submain/CommonNav';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { Row } from 'react-bootstrap';
+import React from 'react';
+
+export default function Type() {
+  const router = useRouter();
+  const { region, type } = router.query as { region: string; type: string };
+
+  type RegionNames = {
+    [key: string]: string;
+  };
+
+  const regionNames: RegionNames = {
+    seoul: '서울',
+    busan: '부산',
+    gangneung: '강릉',
+    gyeongju: '경주',
+    jeonju: '전주',
+    jeju: '제주',
+  };
+
+  type TypeNames = {
+    [key: string]: string;
+  };
+
+  const typesNames: TypeNames = {
+    sightseeing: '관광',
+    culture: '문화',
+    food: '음식',
+    lodgment: '숙소',
+    shopping: '쇼핑',
+  };
+
+  return (
+    <>
+      <Head>
+        <title>
+          {regionNames[region]} - {typesNames[type]}
+        </title>
+      </Head>
+
+      <CommonNav type={type} margin="mt-5" />
+
+      <Row xs={1} sm={2} md={2} lg={3} className="mt-2">
+        {Array.from({ length: 12 }).map((_, i) => {
+          return <ListCard key={i} />;
+        })}
+      </Row>
+    </>
+  );
+}
--- pages/list/[type].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-
-export default function Type() {
-  const router = useRouter();
-  const { type } = router.query;
-
-  return <div>{type}</div>;
-}
