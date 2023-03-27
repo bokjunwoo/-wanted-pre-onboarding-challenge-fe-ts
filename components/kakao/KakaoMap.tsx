157eee5b9955f25d281dfb41b1c8bf5685483830
JUNU <bokjunwoo@gmail.com>
2023-03-27 19:25:24 +0900

    feat : KakaoMap 컴포넌트 구현

---
+++ components/kakao/KakaoMap.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+interface KakaoMapSize {
+  width: string;
+  height: string;
+}
+
+export default function KakaoMap({ width, height }: KakaoMapSize) {
+  const [kakaoLoaded, setKakaoLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      '//dapi.kakao.com/v2/maps/sdk.js?appkey=e79b288ebffab6c35ea1c3d7624e2f3a&libraries=services&autoload=false';
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        setKakaoLoaded(true);
+      });
+    };
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (kakaoLoaded) {
+      const container = document.getElementById('map');
+      const options = {
+        center: new window.kakao.maps.LatLng(37.541, 126.986),
+        level: 7,
+      };
+
+      const map = new window.kakao.maps.Map(container, options);
+      map.setDraggable(false);
+      map.setZoomable(false);
+
+      new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(37.541, 126.986),
+      });
+    }
+  }, [kakaoLoaded]);
+
+  return (
+    <>
+      <Map id="map" width={width} height={height}></Map>
+    </>
+  );
+}
+
+const Map = styled.div<{ width: string; height: string }>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
