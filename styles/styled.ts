2a618e6923294c716bfb51d2bdc2c1f4a2338000
JUNU <bokjunwoo@gmail.com>
2023-03-28 22:32:29 +0900

    style : styled-components 분리

---
+++ styles/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Card } from 'react-bootstrap';
+
+export const Container = styled(Card)`
+  height: 450px;
+  overflow: hidden;
+`;
+
+export const Cursor = styled.span`
+  cursor: pointer;
+`;
