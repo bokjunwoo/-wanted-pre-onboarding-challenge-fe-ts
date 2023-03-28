42cb9ce40866b70d3426c02bcb8480ded0dedc6f
JUNU <bokjunwoo@gmail.com>
2023-03-29 00:06:54 +0900

    feat : _app.tsx react-query setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,25 @@ import type { AppProps } from 'next/app';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import AppLayout from '@/components/layout/AppLayout';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { useState } from 'react';
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
