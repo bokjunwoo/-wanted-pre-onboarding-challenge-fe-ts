ea920236c03f709b3093c8e140ba9c52873c914b
JUNU <bokjunwoo@gmail.com>
2023-03-29 18:08:52 +0900

    fix : meta삭제

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head />
-      <meta
-        httpEquiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
-      />
       <body>
         <Main />
         <NextScript />
