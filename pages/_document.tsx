cb8f1576b66203561fd96e4433338eabe8b37d36
JUNU <bokjunwoo@gmail.com>
2023-03-29 16:36:48 +0900

    feat : Mixed Content 오류 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head />
+      <meta
+        httpEquiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
       <body>
         <Main />
         <NextScript />
