896dd5f64887324f1760fa03fd226e624a3e93e7
JUNU <bokjunwoo@gmail.com>
2023-03-31 23:34:17 +0900

    feat : PlanListCard 컴포넌트 구현

---
+++ components/plan/PlanListCard.tsx
@@ -0,0 +1,37 @@
+import { Container } from '@/styles/styled';
+import React from 'react';
+import { Button, Form, ListGroup } from 'react-bootstrap';
+import useInput from '../hooks/useInput';
+import PlanList from './PlanList';
+
+export default function PlanListCard() {
+  const [search, onChangeSearch] = useInput('', null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container>
+      <Form
+        className="d-flex justify-content-center mt-3"
+        onSubmit={handleSubmit}
+      >
+        <Form.Control
+          type="text"
+          placeholder="검색어를 입력해주세요"
+          className="p-1 w-50 me-2"
+          value={search}
+          onChange={onChangeSearch}
+        />
+        <Button variant="secondary">검색</Button>
+      </Form>
+
+      <ListGroup variant="flush" className="mt-3">
+        {Array.from({ length: 10 }).map((_, i) => {
+          return <PlanList key={i} />;
+        })}
+      </ListGroup>
+    </Container>
+  );
+}
