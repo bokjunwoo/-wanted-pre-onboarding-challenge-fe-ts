941eaddfd29386a7af0f092949ddb599d7820e28
JUNU <bokjunwoo@gmail.com>
2023-03-31 23:35:27 +0900

    feat : plan 페이지 구현

--- pages/plan/[region]/[userId]/[id].tsx
+++ pages/plan/[region]/[userId]/[id].tsx
@@ -1,9 +1,20 @@
+import PlanListCard from '@/components/plan/PlanListCard';
+import PlanMap from '@/components/plan/PlanMap';
+import PlanUserInfo from '@/components/plan/PlanUserInfo';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function PlanUserId() {
   const router = useRouter();
-  const { id } = router.query;
+  const { region, userId } = router.query as { region: string; userId: string };
 
-  return <div>{id}</div>;
+  return (
+    <>
+      <PlanUserInfo userId={userId} />
+
+      <PlanMap region={region} />
+
+      <PlanListCard />
+    </>
+  );
 }
