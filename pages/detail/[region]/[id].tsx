a80dd438c6645a9df1e701472d791fd6e7dcf06a
JUNU <bokjunwoo@gmail.com>
2023-04-03 23:20:30 +0900

    fix : getServerSideProps의 queryClient수정
    기존 fetchQuery로 배포를 할 경우 기존 데이터를 백그라운드에서 가져오기 때문에 없는 상품 방문시에 전에 데이터를 가져오는 현상이 있어 직접 호출하는 fetchQuery로 수정

--- pages/detail/[region]/[id].tsx
+++ pages/detail/[region]/[id].tsx
@@ -121,10 +121,10 @@ export const getServerSideProps = async ({ params }: DetailParamsType) => {
   const queryClient = new QueryClient();
 
   await Promise.all([
-    queryClient.prefetchQuery(['fetchDetail'], () => fetchDetail(region, id)),
-    queryClient.prefetchQuery(['fetchReview'], () => fetchReview(id)),
-    queryClient.prefetchQuery(['fetchReviewLike'], () => fetchReviewLike(id)),
-    queryClient.prefetchQuery(['fetchKoreaAPI'], () => fetchKoreaAPI(id)),
+    queryClient.fetchQuery(['fetchDetail'], () => fetchDetail(region, id)),
+    queryClient.fetchQuery(['fetchReview'], () => fetchReview(id)),
+    queryClient.fetchQuery(['fetchReviewLike'], () => fetchReviewLike(id)),
+    queryClient.fetchQuery(['fetchKoreaAPI'], () => fetchKoreaAPI(id)),
   ]);
 
   return {
