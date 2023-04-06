1cecce4135618244e87aa18a948ec3aa1c45b7bf
JUNU <bokjunwoo@gmail.com>
2023-04-06 23:59:20 +0900

    feat : 리스트페이지 페이지네이션 구성 및 page 쿼리스트링 구현

--- pages/list/[region]/[type].tsx
+++ pages/list/[region]/[type].tsx
@@ -2,7 +2,7 @@ import ListCard from '@/components/list/ListCard';
 import CommonNav from '@/components/list/ListNav';
 import Head from 'next/head';
 import { Row } from 'react-bootstrap';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { regionNames, typesNames } from '@/data/region';
 import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
 import { fetchList } from '@/pages/api/list';
@@ -10,10 +10,12 @@ import { useRouter } from 'next/router';
 import { GetStaticPropsContext } from 'next';
 import { useSearchParams } from 'next/navigation';
 import LoadingSpinner from '@/components/common/LoadingSpinner';
+import PaginatedItems from '@/components/common/PaginatedItems';
 
 export default function Type() {
   const searchParams = useSearchParams();
-  const page = searchParams.get('page');
+
+  const page = Number(searchParams.get('page'));
 
   const router = useRouter();
   const { region, type } = router.query as {
@@ -23,8 +25,22 @@ export default function Type() {
 
   const { data, isLoading } = useQuery({
     queryKey: ['fetchList', region, type, page],
-    queryFn: () => fetchList(region, type, Number(router.query.page)),
+    queryFn: () => fetchList(region, type, page),
   });
+
+  const createQueryString = useCallback(
+    (name: string, value: string | number) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value.toString());
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const handlePageChange = (pageNumber: number) => {
+    router.push('?' + createQueryString('page', pageNumber));
+  };
 
   if (isLoading) return <LoadingSpinner />;
 
@@ -41,6 +57,12 @@ export default function Type() {
           return <ListCard key={data._id} data={data} region={region} />;
         })}
       </Row>
+
+      <PaginatedItems
+        page={page}
+        total={data?.total}
+        handlePageChange={handlePageChange}
+      />
     </>
   );
 }
