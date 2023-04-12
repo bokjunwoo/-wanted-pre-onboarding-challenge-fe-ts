3b295fde59756a04313b50278b1f4b66b2884845
JUNU <bokjunwoo@gmail.com>
2023-04-12 23:02:35 +0900

    fix : api data 수정

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,19 +1,19 @@
 import apiClient from './apiClient';
 
-export const idIsDuplicate = async (id: string) => {
+export const idIsDuplicate = async (email: string) => {
   const response = await apiClient({
     method: 'post',
     url: '/user/register/idcheck',
-    data: { id },
+    data: { email },
   });
   return response.data.idCheck;
 };
 
-export const nickNameIsDuplicate = async (id: string) => {
+export const nickNameIsDuplicate = async (nickName: string) => {
   const response = await apiClient({
     method: 'post',
     url: '/user/register/namecheck',
-    data: { id },
+    data: { nickName },
   });
   return response.data.nameCheck;
 };
