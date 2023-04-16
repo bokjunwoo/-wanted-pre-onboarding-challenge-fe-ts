d78f7d467644be57ca6c7749dec70062ec8f96f2
JUNU <bokjunwoo@gmail.com>
2023-04-17 00:04:41 +0900

    feat : 로컬 로그인 기능 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,17 +4,47 @@ import KakaoLogin from '@/components/kakao/KakaoLogin';
 import { emailValidation, passwordValidation } from '@/utils/sign';
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Card, Badge, Col, Form } from 'react-bootstrap';
 import styled from 'styled-components';
+import { ISignResult } from './api/api';
+import { localLogin } from './api/sign';
+import SignSuccess from '@/components/modal/SignSuccess';
 
 export default function login() {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
   const [email, onChangeEmail] = useInput('', emailValidation);
   const [password, onChangePassword] = useInput('', passwordValidation);
+  const [loginResult, setLoginResult] = useState<ISignResult>({
+    type: 'login',
+    success: false,
+    message: '',
+  });
+  const [show, setShow] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  const onSubmitForm = useCallback(async () => {
+    if (email === '') {
+      emailInputRef.current?.focus();
+      return;
+    }
+
+    if (password === '') {
+      passwordInputRef.current?.focus();
+      return;
+    }
+
+    const response = await localLogin(email, password);
+    const { data } = response;
+    const result: ISignResult = {
+      type: data.type,
+      success: data.success,
+      message: data.message,
+    };
+    setLoginResult(result);
+    setShow(true);
+  }, [email, password]);
 
   return (
     <>
@@ -27,23 +57,31 @@ export default function login() {
           <div className="d-flex mb-5">
             <h4>TripLog</h4>
             <Link href="/signup">
-              <Badge bg="secondary" text="light" className="ms-2 p-1" style={{ fontSize: '.3rem' }}>
+              <Badge
+                bg="secondary"
+                text="light"
+                className="ms-2 p-1"
+                style={{ fontSize: '.3rem' }}
+              >
                 아직 회원이 아니라면?
               </Badge>
             </Link>
           </div>
 
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={onSubmitForm}>
             <Form.Group controlId="email">
               <Form.Label>아이디</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="아이디를 입력해주세요"
                 className="p-3"
+                ref={emailInputRef}
                 value={email}
                 onChange={onChangeEmail}
               />
-              <VisibilityHidden className="text-muted">visibilityHidden</VisibilityHidden>
+              <VisibilityHidden className="text-muted">
+                visibilityHidden
+              </VisibilityHidden>
             </Form.Group>
 
             <Form.Group controlId="password">
@@ -52,13 +90,26 @@ export default function login() {
                 type="password"
                 placeholder="비밀번호를 입력해주세요"
                 className="p-3"
+                ref={passwordInputRef}
                 value={password}
                 onChange={onChangePassword}
               />
-              <VisibilityHidden className="text-muted">visibilityHidden</VisibilityHidden>
+              <VisibilityHidden className="text-muted">
+                visibilityHidden
+              </VisibilityHidden>
             </Form.Group>
 
-            <LocalButton text="로그인" />
+            {loginResult.success === false ? (
+              <div className="text-danger">{loginResult.message}</div>
+            ) : (
+              <SignSuccess
+                show={show}
+                setShow={setShow}
+                result={loginResult}
+              />
+            )}
+
+            <LocalButton text="로그인" onSubmitForm={onSubmitForm} />
 
             <KakaoLogin text="카카오 로그인" />
           </Form>
