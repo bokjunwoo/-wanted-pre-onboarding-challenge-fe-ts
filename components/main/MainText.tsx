cf06404958d3566418de36d77a8e4656aac22f4c
JUNU <bokjunwoo@gmail.com>
2023-05-05 15:38:40 +0900

    feat : 메인 화면 로그인 유무에 따른 메세지 추가

--- components/main/MainText.tsx
+++ components/main/MainText.tsx
@@ -1,11 +1,13 @@
+import { userInfo } from '@/pages/api/sign';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 export default function MainText() {
+  const { data: user } = useQuery(['user'], userInfo);
+
   return (
     <h1 className="fw-bold lh-base mt-5 mx-lg-5 fs-md-6">
-      <span style={{ color: '#198754' }}>
-        여행자
-      </span>
+      <span style={{ color: '#198754' }}>{user ? user : '여행자'}</span>
       님,
       <br></br>TripLog에 오신걸 환영합니다!
     </h1>
