435c02785b0d8926286e584deaf817e0cb4b2f8b
JUNU <bokjunwoo@gmail.com>
2023-05-25 00:06:05 +0900

    test : 쿠키 삭제

--- pages/checklist.tsx
+++ pages/checklist.tsx
@@ -56,14 +56,7 @@ export default function CkecklistUserId({ data: user }: any) {
   );
 }
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext,
-) => {
-  const cookie = context.req ? context.req.headers.cookie : '';
-  axios.defaults.headers.Cookie = '';
-  if (context.req && cookie) {
-    axios.defaults.headers.Cookie = cookie;
-  }
+export const getServerSideProps = async () => {
   const data = await userInfo();
 
   if (!data) {
