94a708f622ff06d70444cf79968f737f958aec81
JUNU <bokjunwoo@gmail.com>
2023-05-24 22:02:46 +0900

    test : checklistContent가 []경우

--- pages/checklist.tsx
+++ pages/checklist.tsx
@@ -42,6 +42,8 @@ export default function CkecklistUserId() {
 
   if (isLoading) return <LoadingSpinner />;
 
+  if(checklistContent.length === 0) return '데이터 없음'
+
   return (
     <>
       <Head>
