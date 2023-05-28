bd6b9d9a7f8f7c2cef0e95751d5ea8e6dbeca4f3
JUNU <bokjunwoo@gmail.com>
2023-05-28 21:52:13 +0900

    feat : HTMLTextAreaElement 타입 추가

--- components/hooks/useInput.tsx
+++ components/hooks/useInput.tsx
@@ -4,7 +4,7 @@ export default (initialValue: string) => {
   const [value, setValue] = useState(initialValue);
 
   const onChange = useCallback(
-    async (e: React.ChangeEvent<HTMLInputElement>) => {
+    async (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       setValue(e.target.value);
     },
     [],
