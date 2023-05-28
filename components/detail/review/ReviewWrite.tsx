9ee86dbd64b08b8184697c419a06af859dabfbf0
JUNU <bokjunwoo@gmail.com>
2023-05-28 21:51:48 +0900

    feat : 리뷰 별점 기능 추가

--- components/detail/review/ReviewWrite.tsx
+++ components/detail/review/ReviewWrite.tsx
@@ -1,3 +1,11 @@
+import useInput from '@/components/hooks/useInput';
+import { IReviewInfo } from '@/pages/api/api';
+import { reviewAdd } from '@/pages/api/review';
+import { userInfo } from '@/pages/api/sign';
+import { Stars } from '@/styles/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import TextareaAutosize from 'react-textarea-autosize';
@@ -8,12 +16,34 @@ interface ReviewWriteProps {
   minRows?: number;
 }
 
+const starArr = [0, 1, 2, 3, 4];
+
 export default function ReviewWrite({
   value,
   autoFocus,
   minRows,
 }: ReviewWriteProps) {
+  const queryClient = useQueryClient();
+
+  const { data: user } = useQuery(['user'], userInfo);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [text, onChangeText, setText] = useInput('');
+  const [starclicked, setStarClicked] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const handleStarClick = (index: number) => {
+    let clickStates = [...starclicked];
+    for (let i = 0; i < 5; i++) {
+      clickStates[i] = i <= index ? true : false;
+    }
+    setStarClicked(clickStates);
+  };
 
   useEffect(() => {
     if (autoFocus && textareaRef.current && value) {
@@ -22,13 +52,42 @@ export default function ReviewWrite({
     }
   }, []);
 
-  const [text, setText] = useState<string | undefined>(value);
-  const onChangeText = useCallback((e: any) => {
-    setText(e.target.value);
+  const mutationAdd = useMutation(['review'], reviewAdd, {
+    onMutate() {
+      if (!user) return;
+      // queryClient.setQueryData<IReviewInfo>(['review'], (data) => {
+
+      // });
+    },
+    onSuccess() {
+      setText('');
+    },
+    onSettled() {
+      queryClient.refetchQueries(['review']);
+    },
+  });
+
+  const onSubmitReview = useCallback((user: string, text: string) => {
+    mutationAdd.mutate({ user, text });
   }, []);
 
   return (
-    <Form className="mt-4">
+    <Form>
+      <Stars className="mt-3 mb-3 justify-content-center">
+        {starArr.map((v, i) => {
+          return (
+            <FontAwesomeIcon
+              key={i}
+              size="xl"
+              icon={faStar}
+              onClick={() => {
+                handleStarClick(v);
+              }}
+              className={starclicked[v] ? 'yellowStar' : undefined}
+            />
+          );
+        })}
+      </Stars>
       <div className="form-floating">
         <TextareaAutosize
           className="form-control"
@@ -41,7 +100,15 @@ export default function ReviewWrite({
         <label htmlFor="floatingTextarea">내용</label>
       </div>
       <div className="d-flex justify-content-end">
-        <Button size="sm" className="mt-2" variant="outline-primary">
+        <Button
+          size="sm"
+          className="mt-2"
+          variant="outline-primary"
+          type="button"
+          onClick={() => {
+            onSubmitReview(user, text);
+          }}
+        >
           등록
         </Button>
       </div>
