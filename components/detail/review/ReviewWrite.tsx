e026a9ec90b600714c01068eff25a896c595d48e
JUNU <bokjunwoo@gmail.com>
2023-05-29 22:20:31 +0900

    feat : 리뷰 작성 AlretModal 기능 추가

--- components/detail/review/ReviewWrite.tsx
+++ components/detail/review/ReviewWrite.tsx
@@ -9,6 +9,8 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import TextareaAutosize from 'react-textarea-autosize';
+import AlretModal from '@/components/modal/AlertModal';
+import { ERROR_MESSAGE } from '@/constants/message';
 
 interface ReviewWriteProps {
   value?: string;
@@ -36,6 +38,8 @@ export default function ReviewWrite({
     false,
     false,
   ]);
+  const [show, setShow] = useState(false);
+  const [message, setMessage] = useState('');
 
   const handleStarClick = (index: number) => {
     let clickStates = [...starclicked];
@@ -67,51 +71,69 @@ export default function ReviewWrite({
     },
   });
 
-  const onSubmitReview = useCallback((user: string, text: string) => {
-    mutationAdd.mutate({ user, text });
-  }, []);
+  const onSubmitReview = useCallback(
+    (user: string, text: string) => {
+      const star = starclicked.filter(Boolean).length;
+      if (star === 0) {
+        setShow(true);
+        setMessage(ERROR_MESSAGE.NO_STAR_RATING);
+        return;
+      }
+      if (text.length === 0) {
+        setShow(true);
+        setMessage(ERROR_MESSAGE.NO_REVIEW_WRITE);
+        return;
+      }
+      mutationAdd.mutate({ user, text });
+    },
+    [starclicked],
+  );
 
   return (
-    <Form>
-      <Stars className="mt-3 mb-3 justify-content-center">
-        {starArr.map((v, i) => {
-          return (
-            <FontAwesomeIcon
-              key={i}
-              size="xl"
-              icon={faStar}
-              onClick={() => {
-                handleStarClick(v);
-              }}
-              className={starclicked[v] ? 'yellowStar' : undefined}
-            />
-          );
-        })}
-      </Stars>
-      <div className="form-floating">
-        <TextareaAutosize
-          className="form-control"
-          id="floatingTextarea"
-          value={text}
-          ref={textareaRef}
-          minRows={minRows}
-          onChange={onChangeText}
-        />
-        <label htmlFor="floatingTextarea">내용</label>
-      </div>
-      <div className="d-flex justify-content-end">
-        <Button
-          size="sm"
-          className="mt-2"
-          variant="outline-primary"
-          type="button"
-          onClick={() => {
-            onSubmitReview(user, text);
-          }}
-        >
-          등록
-        </Button>
-      </div>
-    </Form>
+    <>
+      <Form>
+        <Stars className="mt-3 mb-3 justify-content-center">
+          {starArr.map((v, i) => {
+            return (
+              <FontAwesomeIcon
+                key={i}
+                size="xl"
+                icon={faStar}
+                onClick={() => {
+                  handleStarClick(v);
+                }}
+                className={starclicked[v] ? 'yellowStar' : undefined}
+              />
+            );
+          })}
+        </Stars>
+        <div className="form-floating">
+          <TextareaAutosize
+            className="form-control"
+            id="floatingTextarea"
+            value={text}
+            ref={textareaRef}
+            minRows={minRows}
+            onChange={onChangeText}
+          />
+          <label htmlFor="floatingTextarea">내용</label>
+        </div>
+        <div className="d-flex justify-content-end">
+          <Button
+            size="sm"
+            className="mt-2 mb-4"
+            variant="outline-primary"
+            type="button"
+            onClick={() => {
+              onSubmitReview(user, text);
+            }}
+          >
+            등록
+          </Button>
+        </div>
+      </Form>
+
+      <AlretModal show={show} setShow={setShow} message={message} />
+    </>
   );
 }
