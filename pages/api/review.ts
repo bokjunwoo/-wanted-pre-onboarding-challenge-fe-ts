6c75e09d4022e5deb7263ed9da394f50f26a26eb
JUNU <bokjunwoo@gmail.com>
2023-05-31 01:50:44 +0900

    feat : 리뷰 수정 api 요청 추가

--- pages/api/review.ts
+++ pages/api/review.ts
@@ -1,9 +1,19 @@
+import { IReviewAdd, IReviewEdit } from './api';
 import apiClient from './apiClient';
 
-export const reviewAdd = async (data: { user: string; text: string }) => {
+export const reviewAdd = async (data: IReviewAdd) => {
   const response = await apiClient({
     method: 'post',
     url: '/review/add',
+    data,
+  });
+  return response.data;
+};
+
+export const reviewEdit = async (data: IReviewEdit) => {
+  const response = await apiClient({
+    method: 'post',
+    url: '/review/edit',
     data,
   });
   return response.data;
