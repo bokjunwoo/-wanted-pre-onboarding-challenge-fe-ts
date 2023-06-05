6b87bc7f795be1d8c590f46e6c675689f2112f6a
JUNU <bokjunwoo@gmail.com>
2023-06-05 18:14:55 +0900

    feat : 공공데이터 포털 api cors 요청 프록시 이용

--- pages/api/detail.ts
+++ pages/api/detail.ts
@@ -1,5 +1,4 @@
 import apiClient from './apiClient';
-import axios from 'axios';
 import { IItemInfo, IKoreaAPI, IReviewInfo } from './api';
 
 export const fetchDetail = async (
@@ -29,15 +28,14 @@ export const fetchReviewLike = async (id: string): Promise<string[]> => {
     url: `/detail/${id}`,
     params: { id },
   });
-  return response.data.likeuser;
+  return response.data;
 };
 
 export const fetchKoreaAPI = async (id: string): Promise<IKoreaAPI> => {
-  const response = await axios.get(
-    `https://apis.data.go.kr/B551011/KorService/detailCommon?serviceKey=rfaoGpiapHFqOcUT6bqfERRxy1WVxzOdOpEC3ChyAFPEfONdSMdRVNETTJKRhqTbPuZ2krpG2mQJMXDbyG74RA%3D%3D&MobileOS=ETC&MobileApp=AppTest&_type=json&contentId=${id}&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y`,
-  );
-  return {
-    homepage: response.data.response.body.items.item[0].homepage,
-    overview: response.data.response.body.items.item[0].overview,
-  };
+  const response = await apiClient({
+    method: 'post',
+    url: `/detail/${id}`,
+    params: { id },
+  });
+  return response.data;
 };
