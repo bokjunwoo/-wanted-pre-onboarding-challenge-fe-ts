8caf6e2accbde02a08298c78e40b0d196c867010
JUNU <bokjunwoo@gmail.com>
2023-06-17 23:57:32 +0900

    feat : 체크리스트 작성 메세지 toast 기능 추가

--- components/checklist/ChecklistAccordionTitle.tsx
+++ components/checklist/ChecklistAccordionTitle.tsx
@@ -1,11 +1,12 @@
 import { checklistAdd } from '@/pages/api/checklist';
 import { Checklist, ChecklistContent } from '@/pages/checklist/[userId]';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Accordion, Form, InputGroup, Button } from 'react-bootstrap';
 import useInput from '../hooks/useInput';
 import { userInfo } from '@/pages/api/sign';
 import ChecklistAccordionItem from './ChecklistAccordionItem';
+import ToastMessage from '../toast/ToastMessage';
 
 export default function ChecklistAccordionTitle({
   title,
@@ -17,6 +18,7 @@ export default function ChecklistAccordionTitle({
   const { data: user } = useQuery(['user'], userInfo);
 
   const [text, onChangeText, setText] = useInput('');
+  const [toast, setToast] = useState(false);
 
   const mutationAdd = useMutation(['checklist'], checklistAdd, {
     onMutate() {
@@ -49,47 +51,59 @@ export default function ChecklistAccordionTitle({
 
   const onSubmitItem = useCallback(
     (title: string, item: string) => {
-      if (text.length === 0) return alert('글 작성필요');
+      if (text.length === 0) {
+        setToast(true);
+        return;
+      }
       mutationAdd.mutate({ title, item, user });
     },
     [text, mutationAdd, user],
   );
 
   return (
-    <Accordion.Item eventKey={index.toString()}>
-      <Accordion.Header>{title}</Accordion.Header>
-      <Accordion.Body className="text-start">
-        <Form>
-          {items.map((v, i) => {
-            return (
-              <ChecklistAccordionItem
-                item={v.item}
-                checked={v.checked}
-                title={title}
-                key={i}
+    <>
+      <Accordion.Item eventKey={index.toString()}>
+        <Accordion.Header>{title}</Accordion.Header>
+        <Accordion.Body className="text-start">
+          <Form>
+            {items.map((v, i) => {
+              return (
+                <ChecklistAccordionItem
+                  item={v.item}
+                  checked={v.checked}
+                  title={title}
+                  key={i}
+                />
+              );
+            })}
+            <InputGroup className="mt-3">
+              <Form.Control
+                type="text"
+                placeholder="아이템 추가하기"
+                value={text}
+                onChange={onChangeText}
               />
-            );
-          })}
-          <InputGroup className="mt-3">
-            <Form.Control
-              type="text"
-              placeholder="아이템 추가하기"
-              value={text}
-              onChange={onChangeText}
-            />
-            <Button
-              variant="success"
-              id="button-addon2"
-              type="button"
-              onClick={() => {
-                onSubmitItem(title, text);
-              }}
-            >
-              추가
-            </Button>
-          </InputGroup>
-        </Form>
-      </Accordion.Body>
-    </Accordion.Item>
+              <Button
+                variant="success"
+                id="button-addon2"
+                type="button"
+                onClick={() => {
+                  onSubmitItem(title, text);
+                }}
+              >
+                추가
+              </Button>
+            </InputGroup>
+          </Form>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <ToastMessage
+        message="글 작성이 필요합니다."
+        show={toast}
+        setShow={setToast}
+        variant="danger"
+      />
+    </>
   );
 }
