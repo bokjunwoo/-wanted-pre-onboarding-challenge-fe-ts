5ab4d76b20f47ff596d16938a4a64c64c10ee74d
JUNU <bokjunwoo@gmail.com>
2023-06-20 22:29:41 +0900

    feat : /mypage 기본 구성

--- pages/mypage/[userId].tsx
+++ pages/mypage/[userId].tsx
@@ -1,9 +1,48 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import dynamic from 'next/dynamic';
+
+const MypageNavWithNoSSR = dynamic(import('@/components/mypage/MypageNav'), {
+  ssr: false,
+});
 
 export default function MypageUserId() {
   const router = useRouter();
   const { userId } = router.query;
 
-  return <div>{userId}</div>;
+  const [activeKey, setActiveKey] = useState('');
+
+  useEffect(() => {
+    const storedActiveKey = sessionStorage.getItem('activeTab');
+    const isValidKey = ['plan', 'checklist', 'ledger', 'review'].includes(
+      storedActiveKey || '',
+    );
+
+    setActiveKey(isValidKey ? storedActiveKey || 'plan' : 'plan');
+  }, []);
+
+  const handleTabSelect = (eventKey: string | null) => {
+    setActiveKey(eventKey || 'plan');
+    sessionStorage.setItem('activeTab', eventKey || 'plan');
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{`${userId} - 마이페이지`}</title>
+      </Head>
+
+      <Row className="m-auto mt-5">
+        <Col md={12} lg={3}>
+          <MypageNavWithNoSSR
+            activeKey={activeKey}
+            handleTabSelect={handleTabSelect}
+          />
+        </Col>
+        <Col md={12} lg={9}></Col>
+      </Row>
+    </div>
+  );
 }
