d4a4cddc3fa9395295d481664fbd4ab7db11b2ea
JUNU <bokjunwoo@gmail.com>
2023-06-26 23:09:45 +0900

    feat : 마이페이지 리뷰 섹션 컴포넌트 추가 및 로딩 처리

---
+++ components/mypage/MypageReviewSection.tsx
@@ -0,0 +1,24 @@
+import { useUserReviewData } from '@/usequery/useUserReview';
+import React from 'react';
+import ParentLoading from '../common/ParentLoading';
+import MypageReviewCard from './MypageReviewCard';
+
+export default function MypageReviewSection() {
+  const { userReview, userReviewLoading } = useUserReviewData();
+
+  if (userReviewLoading) {
+    return <ParentLoading />;
+  }
+
+  if (!userReview) {
+    return <div>작성한 리뷰가 없습니다.</div>;
+  }
+
+  return (
+    <>
+      {userReview?.map((v) => (
+        <MypageReviewCard key={v._id} review={v} />
+      ))}
+    </>
+  );
+}
