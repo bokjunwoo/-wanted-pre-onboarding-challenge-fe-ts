da6cd4e413b797174baa99891bee94e332f58e3d
JUNU <bokjunwoo@gmail.com>
2023-06-27 23:24:16 +0900

    feat : MypageReviewCard 컴포넌트 구현

--- components/mypage/MypageReviewCard.tsx
+++ components/mypage/MypageReviewCard.tsx
@@ -1,6 +1,27 @@
 import { IReviewInfo } from '@/pages/api/api';
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 export default function MypageReviewCard({ review }: { review: IReviewInfo }) {
-  return <div>{review.content}</div>;
+  const stars = [0, 0, 0, 0, 0];
+
+  for (let i = 0; i < review.star; i++) {
+    stars[i] = 1;
+  }
+
+  return (
+    <Card border="secondary" className="m-2">
+      <Card.Header>
+        {review.title}{' '}
+        {stars.map((star, i) => (
+          <span key={i} style={{ color: star === 0 ? 'gray' : '#ffd400' }}>
+            ★
+          </span>
+        ))}
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>{review.content}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
