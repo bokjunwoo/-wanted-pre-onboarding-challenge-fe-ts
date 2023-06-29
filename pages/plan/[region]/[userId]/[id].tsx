a108d06cd962ab846c7b4b3e5470bc5781542907
JUNU <bokjunwoo@gmail.com>
2023-06-29 21:29:49 +0900

    fix : plan page 구조 수정

--- pages/plan/[region]/[userId]/[id].tsx
+++ pages/plan/[region]/[userId]/[id].tsx
@@ -1,20 +1,55 @@
-import PlanListCard from '@/components/plan/PlanListCard';
-import PlanMap from '@/components/plan/PlanMap';
-import PlanUserInfo from '@/components/plan/PlanUserInfo';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+import UserInfo from '@/components/common/UserInfo';
+import PlanKakaoMap from '@/components/kakao/PlanKakaoMap';
+import PlanList from '@/components/plan/PlanList';
+import PlanListForm from '@/components/plan/PlanListForm';
+import { userInfo } from '@/pages/api/sign';
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { Accordion, ListGroup } from 'react-bootstrap';
 
 export default function PlanUserId() {
   const router = useRouter();
-  const { region, userId } = router.query as { region: string; userId: string };
+  const { region } = router.query as { region: string };
+
+  const { data: user, isLoading: userLoading } = useQuery(['user'], userInfo);
+
+  if (userLoading) return <LoadingSpinner />;
 
   return (
     <>
-      <PlanUserInfo userId={userId} />
+      <div className="mt-5">
+        <UserInfo user={user} message="여행계획 세우기 ✏️" />
+      </div>
 
-      <PlanMap region={region} />
+      <div className="mt-2">
+        <h2 className="mb-2">📆 여행 기간</h2>
+        <h6>2023년 03월 04일 ~ 2023년 03월 05일</h6>
+      </div>
 
-      <PlanListCard />
+      <Accordion defaultActiveKey="0" alwaysOpen className="mt-3">
+        {Array.from({ length: 3 }).map((_, i) => {
+          return (
+            <Accordion.Item eventKey={i.toString()} key={i}>
+              <Accordion.Header>
+                <h4 className="m-0">{i + 1}일차</h4>
+              </Accordion.Header>
+              <Accordion.Body>
+                <PlanKakaoMap region={region} idx={i} />
+
+                <PlanListForm />
+
+                <ListGroup variant="flush" className="mt-3">
+                  {Array.from({ length: 3 }).map((_, i) => {
+                    return <PlanList key={i} />;
+                  })}
+                </ListGroup>
+              </Accordion.Body>
+            </Accordion.Item>
+          );
+        })}
+      </Accordion>
     </>
   );
 }
