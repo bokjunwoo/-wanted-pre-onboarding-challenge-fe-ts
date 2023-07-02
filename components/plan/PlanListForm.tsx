249c1a8ee0188f6e9b4d51a2507c58d5e272a533
JUNU <bokjunwoo@gmail.com>
2023-07-02 23:17:11 +0900

    feat : 검색 기능 구현

--- components/plan/PlanListForm.tsx
+++ components/plan/PlanListForm.tsx
@@ -1,33 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import useInput from '../hooks/useInput';
+import ToastMessage from '../toast/ToastMessage';
+import SearchModal from '../modal/SearchModal';
+import { useRouter } from 'next/router';
+import { useSearchData } from '@/usequery/useSearch';
 
 export default function PlanListForm() {
-  // const searchHelper = async (search: string) => {
-  //   if (search.length === 0)
-  //     return { message: '검색어를 입력해주세요', success: false };
-  //   return { message: '', success: true };
-  // };
+  const router = useRouter();
+  const { region } = router.query as { region: string };
 
-  const [search, onChangeSearch] = useInput('');
+  const searchHelper = () => {
+    if (search.length === 0) {
+      setToastShow(true);
+      return;
+    }
+    setSearchShow(true);
+  };
+
+  const [search, onChangeSearch, setSearch] = useInput('');
+  const [toastShow, setToastShow] = useState(false);
+  const [searchShow, setSearchShow] = useState(false);
+
+  const { searchData, searchDataLoading } = useSearchData(region, search);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    searchHelper();
   };
 
   return (
-    <Form
-      className="d-flex justify-content-center align-items-center"
-      onSubmit={handleSubmit}
-    >
-      <Form.Control
-        type="text"
-        placeholder="검색어를 입력해주세요"
-        className="p-1 w-50 me-2"
-        value={search}
-        onChange={onChangeSearch}
+    <>
+      <Form
+        className="d-flex justify-content-center align-items-center"
+        onSubmit={handleSubmit}
+      >
+        <Form.Control
+          type="text"
+          placeholder="검색어를 입력해주세요"
+          className="p-1 w-50 me-2"
+          value={search}
+          onChange={onChangeSearch}
+        />
+        <Button variant="secondary" type="submit" size="sm">
+          검색
+        </Button>
+      </Form>
+
+      <ToastMessage
+        show={toastShow}
+        setShow={setToastShow}
+        message="검색어를 입력해주세요."
+        variant="danger"
       />
-      <Button variant="secondary">검색</Button>
-    </Form>
+      <SearchModal show={searchShow} setShow={setSearchShow} />
+    </>
   );
 }
