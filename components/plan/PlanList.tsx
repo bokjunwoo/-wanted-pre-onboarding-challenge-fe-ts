b4594265c0bea4699c10c54ad90f399a836a88da
JUNU <bokjunwoo@gmail.com>
2023-07-03 19:48:33 +0900

    feat : PlanList Props 구현

--- components/plan/PlanList.tsx
+++ components/plan/PlanList.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import { CloseButton, ListGroup } from 'react-bootstrap';
 import { NumberBox } from '@/styles/styled';
+import { getAverageStar } from '@/utils/detailHelper';
 
-export default function PlanList({ number }: { number: number }) {
+interface IPlanList {
+  number?: number;
+  title: string;
+  star: {
+    star: number;
+  }[];
+  addr1: string;
+  like: number;
+}
+
+export default function PlanList({
+  number,
+  title,
+  star,
+  addr1,
+  like,
+}: IPlanList) {
+  const averageStar = getAverageStar(star);
+
   return (
     <ListGroup.Item className="ps-2 pe-2">
       <div className="d-flex justify-content-between">
         <div className="d-flex">
           <div className="d-flex align-items-center">
-            <NumberBox>{number + 1}</NumberBox>
+            {number && <NumberBox>{number}</NumberBox>}
           </div>
 
           <div className="ms-2">
-            <span>경복궁</span>
+            <span className="fs-5">{title}</span>{' '}
+            <span className="text-muted">({addr1})</span>
             <br />
-            <span>⭐️⭐️⭐️⭐️⭐️</span>
-            <span className="text-muted ms-1">23.03.03</span>
+            <span style={{ color: '#ffd400' }}>★</span>{' '}
+            <span className="text-muted">({averageStar})</span> <span>❤️</span>{' '}
+            <span className="text-muted">({like})</span>
           </div>
         </div>
 
-        <div className="d-flex align-items-center">
-          <CloseButton />
-        </div>
+        {number && (
+          <div className="d-flex align-items-center">
+            <CloseButton />
+          </div>
+        )}
       </div>
     </ListGroup.Item>
   );
