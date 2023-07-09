ad64ae6636b340c1ebebf14f36d67dd2f218fe66
JUNU <bokjunwoo@gmail.com>
2023-07-09 20:20:29 +0900

    feat : 여행계획 저장 api 요청

--- pages/api/plan.ts
+++ pages/api/plan.ts
@@ -1,3 +1,4 @@
+import { DateObject } from '@/atom/planSelector';
 import { ISearchData } from './api';
 import apiClient from './apiClient';
 
@@ -5,6 +6,15 @@ export const searchTitle = async (data: ISearchData) => {
   const response = await apiClient({
     method: 'post',
     url: '/plan/search',
+    data,
+  });
+  return response.data;
+};
+
+export const planAdd = async (data: DateObject) => {
+  const response = await apiClient({
+    method: 'post',
+    url: '/plan/add',
     data,
   });
   return response.data;
