38f0bb8694eef94d3fec41819a0386261ce333cc
JUNU <bokjunwoo@gmail.com>
2023-07-23 20:18:37 +0900

    feat : 이미지 저장 기능 추가

--- components/common/UserImageUpload.tsx
+++ components/common/UserImageUpload.tsx
@@ -2,10 +2,12 @@ import { userErrorState } from '@/atom/userErrorSelector';
 import { userImageState } from '@/atom/userImageSelector';
 import { userInfo } from '@/pages/api/sign';
 import { useQuery } from '@tanstack/react-query';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import UserImage from './UserImage';
+import { userImageSave } from '@/pages/api/image';
+import ToastMessage from '../toast/ToastMessage';
 
 export default function UserImageUpload() {
   const [userImage, setuserImage] = useRecoilState(userImageState);
@@ -13,33 +15,59 @@ export default function UserImageUpload() {
 
   const { data: user } = useQuery(['user'], userInfo);
 
+  const [uploadSave, setUploadSave] = useState(false);
+  const [toastShow, setToastShow] = useState(false);
+
   const onClcikCancel = useCallback(() => {
     setuserImage('');
   }, [setuserImage]);
 
+  const onSubmitImage = useCallback(() => {
+    userImageSave({ user: user, image: userImage }).then((res) => {
+      setUploadSave(res);
+      setToastShow(res);
+    });
+  }, [user, userImage]);
+
   return (
-    <div className="d-flex flex-column align-items-center">
-      <UserImage width="150px" height="150px" />
+    <>
+      <div className="d-flex flex-column align-items-center">
+        <UserImage width="150px" height="150px" />
 
-      {userImage !== '' && (
-        <div className="d-flex justify-content-center mt-2 mb-2">
-          <Button size="sm" className="ms-2 me-2" variant="outline-primary">
-            저장
-          </Button>
-          <Button
-            size="sm"
-            className="ms-2 me-2"
-            variant="outline-danger"
-            onClick={onClcikCancel}
-          >
-            취소
-          </Button>
-        </div>
-      )}
+        {userImage !== '' && !uploadSave && (
+          <div className="d-flex justify-content-center mt-2 mb-2">
+            <Button
+              size="sm"
+              className="ms-2 me-2"
+              variant="outline-danger"
+              onClick={onClcikCancel}
+            >
+              취소
+            </Button>
+            <Button
+              size="sm"
+              className="ms-2 me-2"
+              variant="outline-primary"
+              onClick={onSubmitImage}
+            >
+              저장
+            </Button>
+          </div>
+        )}
 
-      {userError !== '' && <div className="text-danger mt-2">{userError}</div>}
+        {userError !== '' && (
+          <div className="text-danger mt-2">{userError}</div>
+        )}
 
-      <p className="fs-3 text-center text-success fw-bold m-2">{user}</p>
-    </div>
+        <p className="fs-3 text-center text-success fw-bold m-2">{user}</p>
+      </div>
+
+      <ToastMessage
+        show={toastShow}
+        setShow={setToastShow}
+        message="이미지 저장이 완료되었어요."
+        variant="primary"
+      />
+    </>
   );
 }
