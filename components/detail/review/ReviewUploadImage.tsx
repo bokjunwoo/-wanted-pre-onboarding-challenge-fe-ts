8625ce8d660f3bc719589d91f6dc1bd9eab94d54
JUNU <bokjunwoo@gmail.com>
2023-07-27 21:33:57 +0900

    feat : 리뷰 업로드 사진 삭제 기능 추가

--- components/detail/review/ReviewUploadImage.tsx
+++ components/detail/review/ReviewUploadImage.tsx
@@ -1,25 +1,56 @@
 import { reviewImageState } from '@/atom/reviewImageSelector';
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useCallback } from 'react';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 export default function ReviewUploadImage() {
-  const reviewImage = useRecoilValue(reviewImageState);
+  const [reviewImage, setReviewImage] = useRecoilState(reviewImageState);
+
+  const deleteImage = useCallback(
+    (imageName: string) => {
+      setReviewImage((prevReviewImage) =>
+        prevReviewImage.filter((v) => v !== imageName),
+      );
+    },
+    [setReviewImage],
+  );
 
   return (
     <div className="d-flex">
       {reviewImage.map((v) => {
         return (
-          <ReviewImageSize src={`http://localhost:4000/${v}`} alt={v} key={v} />
+          <FlexBox key={v}>
+            <ReviewImageSize src={`http://localhost:4000/${v}`} alt={v} />
+            <ImageDelete onClick={() => deleteImage(v)}>
+              <FontAwesomeIcon icon={faX} />
+            </ImageDelete>
+          </FlexBox>
         );
       })}
     </div>
   );
 }
 
+const FlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #cccccc;
+  margin: 10px 10px 0px 0px;
+`;
+
 const ReviewImageSize = styled.img`
   width: 100px;
   height: 100px;
-  border: 1px solid #cccccc;
-  margin: 10px 10px 0px 0px;
+`;
+
+const ImageDelete = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 30px;
+  border-top: 1px solid #cccccc;
+  cursor: pointer;
 `;
