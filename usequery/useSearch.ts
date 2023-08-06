8f1523e9ff72310a73c5e1a4f8a38c6586181488
JUNU <bokjunwoo@gmail.com>
2023-08-06 23:24:43 +0900

    refactor : useSearchData 반환 타입 수정

--- usequery/useSearch.ts
+++ usequery/useSearch.ts
@@ -6,8 +6,8 @@ import { AxiosError } from 'axios';
 export const useSearchData = (
   region: string,
   search: string,
-): QueryObserverResult<ISearchDataInfo, AxiosError> => {
-  return useQuery<ISearchDataInfo, AxiosError>({
+): QueryObserverResult<ISearchDataInfo[], AxiosError> => {
+  return useQuery<ISearchDataInfo[], AxiosError>({
     queryKey: ['searchTitle', region, search],
     queryFn: () => searchTitle({ region, search }),
     enabled: false, // 초기에 요청 비활성화
