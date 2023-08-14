672a631d5e060a3efc1c0bb154ee8927b3c43703
JUNU <bokjunwoo@gmail.com>
2023-08-14 21:03:24 +0900

    fix : page query 추가

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,11 +1,9 @@
+import axios from 'axios';
 import { ISearchData } from './api';
-import apiClient from './apiClient';
 
-export const searchTitle = async (data: ISearchData) => {
-  const response = await apiClient({
-    method: 'get',
-    url: `/search?region=${data.region}?title${data.search}`,
-    params: data,
-  });
+export const loadsearchTitle = async (data: ISearchData) => {
+  const response = await axios.get(
+    `/search?region=${data.region}&title=${data.search}&page=${data.page}`,
+  );
   return response.data;
 };
