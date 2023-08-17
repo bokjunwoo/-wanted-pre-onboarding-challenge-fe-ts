163ba79ccba64feeb07e16765c2ec055c3ad923c
JUNU <bokjunwoo@gmail.com>
2023-08-17 20:49:22 +0900

    feat : 검색기능 추가

--- components/search/SearchForm.tsx
+++ components/search/SearchForm.tsx
@@ -1,13 +1,52 @@
 import { Input } from '@/styles/styled';
-import React from 'react';
+import { useSearchParams } from 'next/navigation';
+import React, { useCallback } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import useInput from '../hooks/useInput';
+import { useRouter } from 'next/router';
 
 export default function SearchForm() {
-  return (
-    <Form className="d-flex align-items-center">
-      <Input type="text" className="me-2" />
+  const searchParams = useSearchParams();
 
-      <Button variant="outline-success">검색</Button>
-    </Form>
+  const router = useRouter();
+
+  const [value, onChangeValue, setValue] = useInput('');
+
+  const handleTitleChange = () => {
+    const queryString = createQueryString('title', value);
+    router.push('?' + queryString);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleTitleChange();
+    setValue('');
+  };
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  return (
+    <>
+      <Form className="d-flex align-items-center" onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          className="me-2"
+          value={value}
+          onChange={onChangeValue}
+        />
+
+        <Button variant="outline-success" type="submit">
+          검색
+        </Button>
+      </Form>
+    </>
   );
 }
