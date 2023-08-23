1c6cec5c00a07e19c0efaf3e532ca2410533dcaf
JUNU <bokjunwoo@gmail.com>
2023-08-23 23:57:09 +0900

    refactor : useQuery를 통한 데이터 요청으로 리팩토링

--- components/search/SearchBestList.tsx
+++ components/search/SearchBestList.tsx
@@ -1,24 +1,24 @@
-import axios from 'axios';
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
 import ListCard from '../list/ListCard';
 import { IDetailInfo } from '@/pages/api/api';
 import { Row } from 'react-bootstrap';
 import { regionNames } from '@/data/region';
+import { loadSearchBest } from '@/pages/api/search';
+import { useQuery } from '@tanstack/react-query';
 
 export default function SearchBestList() {
   const searchParams = useSearchParams();
 
   const region = searchParams.get('region') as string;
 
-  const [data, setData] = useState<IDetailInfo[]>([]);
+  const { data, isLoading } = useQuery<IDetailInfo[]>(
+    ['bestSearchtData', region],
+    () => loadSearchBest(region),
+  );
 
-  useEffect(() => {
-    axios
-      .post('http://localhost:4000/search/best', { region })
-      .then((res) => setData(res.data.data))
-      .catch((error) => console.error(error));
-  }, [region]);
+  if (isLoading) {
+    return <p>로딩중...</p>;
+  }
 
   return (
     <>
@@ -29,7 +29,7 @@ export default function SearchBestList() {
           </h1>
 
           <Row xs={1} sm={2} md={2} lg={3} className="mt-2">
-            {data.map((v) => {
+            {data?.map((v) => {
               return <ListCard key={v._id} data={v} region={region} />;
             })}
           </Row>
@@ -39,7 +39,7 @@ export default function SearchBestList() {
           <h1 className="fs-2 mt-3">✨ 트랜디한 트립로그의 Pick!</h1>
 
           <Row xs={1} sm={2} md={2} lg={3} className="mt-2">
-            {data.map((v) => {
+            {data?.map((v) => {
               return (
                 <ListCard key={v._id} data={v} region={v.region as string} />
               );
