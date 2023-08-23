d4b5c576c2bed32f859b2589cd2fda502b8ff513
JUNU <bokjunwoo@gmail.com>
2023-08-23 23:56:36 +0900

    feat : loadSearchBest api 요청 추가

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import { ISearchData } from './api';
+import apiClient from './apiClient';
 
 export const loadsearchTitle = async (data: ISearchData) => {
   const response = await axios.get(
     `/search?region=${data.region}&title=${data.search}&page=${data.page}`,
   );
   return response.data;
+};
+
+export const loadSearchBest = async (region: string) => {
+  const response = await apiClient({
+    method: 'post',
+    url: '/search/best',
+    data: { region },
+  });
+  return response.data.data;
 };
